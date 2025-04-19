8b28c38a55654b780ff9f7576e3216c4705eca49
Gabriel Sousa <gabrielsousa7418@gmail.com>
2025-04-19 10:15:31 -0300

    Commit
    Biblioteca rellax.js implementada

--- js/script.js
+++ js/script.js
@@ -1,53 +1,33 @@
-const floatingElements = document.querySelectorAll('.floating-img');
-
-  // Inicializa posições Y
-const positions = Array.from(floatingElements).map((el, i) => ({
-    el: el,
-    top: parseFloat(getComputedStyle(el).top),
-    speed: 0.2 + Math.random() * 0.3 // velocidades diferentes
-}));
-
-function animate() {
-    positions.forEach(obj => {
-      obj.top += obj.speed;
-      if (obj.top > window.innerHeight + 100) {
-        obj.top = -150; // reinicia acima da tela
-      }
-      obj.el.style.top = `${obj.top}px`;
-});
-
-requestAnimationFrame(animate);
-}
-
-animate();
-
 // Função para iniciar o contador
 function startCounter(element, start, end, duration, prefix = '') {
-    let range = end - start;
-    let current = start;
-    let increment = end > start ? 1 : -1;
-    let stepTime = Math.abs(Math.floor(duration / range));
-    let timer = setInterval(() => {
-        current += increment;
-        element.textContent = prefix + current;
-        if (current == end) {
-            clearInterval(timer);
-        }
-    }, stepTime);
+  let range = end - start;
+  let current = start;
+  let increment = end > start ? 1 : -1;
+  let stepTime = Math.abs(Math.floor(duration / range));
+  let timer = setInterval(() => {
+      current += increment;
+      element.textContent = prefix + current;
+      if (current == end) {
+          clearInterval(timer);
+      }
+  }, stepTime);
 }
 
 // Configurar Intersection Observer para detectar quando os contadores entram na tela
 let observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            startCounter(document.getElementById('c1'), 0, 500, 2000, '+');
-            startCounter(document.getElementById('c2'), 0, 10, 2000);
-            observer.disconnect(); // Para garantir que o contador só inicia uma vez
-        }
-    });
+  entries.forEach(entry => {
+      if (entry.isIntersecting) {
+          startCounter(document.getElementById('c1'), 0, 500, 2000, '+');
+          startCounter(document.getElementById('c2'), 0, 10, 2000);
+          observer.disconnect(); // Para garantir que o contador só inicia uma vez
+      }
+  });
 }, { threshold: 0.5 });
 
-// Observar a seção onde os contadores estão
+
+var rellax = new Rellax('.rellax');
+
+// ----------- SWIPER -----------
 observer.observe(document.getElementById('counter-section'));
 
 
